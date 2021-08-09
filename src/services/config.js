769025e18833cccde2943fd7fb8f846413de3e89
AmonH <hzu888918@126.com>
2021-08-09 17:04:08 +0800

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = "http://123.207.32.32:9001"; //开发环境
-const proBaseURL = "http://123.207.32.32:9001"; //生产环境
+const devBaseURL = "https://music.163.com/"; //开发环境
+const proBaseURL = "https://music.163.com/"; //生产环境
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
 
 export const TIMEOUT = 5000;
